31e7048a003a1851d3c0782df7606974ad125b9f
Vlad <vladbozhko003@gmail.com>
2023-11-06 22:28:37 +0200

    testing-0: ver1

--- src/main.rs
+++ src/main.rs
@@ -57,108 +57,7 @@ impl PlayField {
         (self.numb_1deck,self.numb_2deck,self.numb_3deck,self.numb_4deck)
     }
 
-    fn place_ship(&mut self,selection:(i32,i32,i32,i32))->Result<(),()>{
-        let deck_num = match Self::check_ship_deck(selection) {
-            Ok(res) => res,
-            Err(_) => {
-                return Err(());
-            }
-        };
-
-        match deck_num {
-            4=>{
-                if self.numb_4deck<MAX_4DECK{
-                   self.set_ship_helper(selection)?; 
-                   self.numb_4deck+=1;
-                }
-            },
-            3=>{
-                if self.numb_3deck<MAX_3DECK{
-                   self.set_ship_helper(selection)?;
-                   self.numb_3deck+=1;
-                }
-            },
-            2=>{
-                if self.numb_2deck<MAX_2DECK{
-                   self.set_ship_helper(selection)?; 
-                   self.numb_2deck+=1;
-                }
-            },
-            1=>{
-                if self.numb_1deck<MAX_1DECK{
-                   self.set_ship_helper(selection)?; 
-                   self.numb_1deck+=1; 
-                }
-            },
-            _=>(),
-        };
-
-        Ok(())
-    }
-    fn check_ship_deck(selection: (i32, i32, i32, i32)) -> Result<i32, ()> {
-        let height = selection.2 - selection.0;
-        let width = selection.3 - selection.1;
     
-        if height == 0 {
-            return Ok(width + 1);
-        }
-        if width == 0 {
-            return Ok(height + 1);
-        }
-        Err(())
-    }
-    fn check_surroundings_and_intersection(&mut self, selection: (i32, i32, i32, i32)) -> bool {
-        let (mut rt, mut cl, mut rb, mut cr) = (
-            selection.0 - 1,
-            selection.1 - 1,
-            selection.2 + 1,
-            selection.3 + 1,
-        );
-    
-        if rt < 0 {
-            rt = 0;
-        }
-        if cl < 0 {
-            cl = 0
-        }
-        if rb >= 10 {
-            rb = 9;
-        }
-        if cr >= 10 {
-            cr = 9;
-        }
-    
-        for i in rt..rb + 1 {
-            for j in cl..cr + 1 {
-                if self.field[i as usize][j as usize] == 1 {
-                    return true;
-                }
-            }
-        }
-        false
-    }
-    
-    fn set_ship_helper(&mut self, selection: (i32, i32, i32, i32)) -> Result<(), ()> {
-        let is_hor = selection.0 == selection.2;
-        let is_ver = selection.1 == selection.3;
-    
-        if (!((is_hor) | (is_ver))) || self.check_surroundings_and_intersection(selection) {
-            return Err(());
-        }
-    
-        if is_hor {
-            for i in selection.1..selection.3 + 1 {
-                self.field[selection.0 as usize][i as usize] = 1;
-            }
-        }
-        if is_ver {
-            for i in selection.0..selection.2 + 1 {
-                self.field[i as usize][selection.1 as usize] = 1;
-            }
-        }
-        Ok(())
-    }
-
     fn strike(&mut self, position:(u8,u8))->Result<bool,()>{
         let (x,y) = position;
         let val = self.field[x as usize][y as usize]; 
@@ -175,6 +74,122 @@ impl PlayField {
             _=> return Err(())
         }
     }
+
+
+
+    fn place_ship(play_field: &mut PlayField,selection:(i32,i32,i32,i32))->Result<(),()>{
+        
+        
+        let check_ship_deck = |selection: (i32, i32, i32, i32)| -> Result<i32, ()> {
+            let height = selection.2 - selection.0;
+            let width = selection.3 - selection.1;
+        
+            if height == 0 {
+                return Ok(width + 1);
+            }
+            if width == 0 {
+                return Ok(height + 1);
+            }
+            Err(())
+        };
+        
+        let check_surroundings_and_intersection = |selection: (i32, i32, i32, i32),field: &[[u8;10];10] |->bool{
+            let (mut rt, mut cl, mut rb, mut cr) = (
+                selection.0 - 1,
+                selection.1 - 1,
+                selection.2 + 1,
+                selection.3 + 1,
+            );
+        
+            if rt < 0 {
+                rt = 0;
+            }
+            if cl < 0 {
+                cl = 0
+            }
+            if rb >= 10 {
+                rb = 9;
+            }
+            if cr >= 10 {
+                cr = 9;
+            }
+        
+            for i in rt..rb + 1 {
+                for j in cl..cr + 1 {
+                    if field[i as usize][j as usize] == 1 {
+                        return true;
+                    }
+                }
+            }
+            false
+        };
+        
+        
+        let set_ship_helper = |selection: (i32, i32, i32, i32),field: &mut [[u8;10];10]| -> Result<(), ()> {
+        
+            let is_hor = selection.0 == selection.2;
+            let is_ver = selection.1 == selection.3;
+        
+            if (!((is_hor) | (is_ver))) || check_surroundings_and_intersection(selection,field) {
+                return Err(());
+            }
+        
+            if is_hor {
+                for i in selection.1..selection.3 + 1 {
+                    field[selection.0 as usize][i as usize] = 1;
+                }
+            }
+            if is_ver {
+                for i in selection.0..selection.2 + 1 {
+                    field[i as usize][selection.1 as usize] = 1;
+                }
+            }
+            Ok(())
+        };
+        
+        
+        let deck_num = match check_ship_deck(selection) {
+            Ok(res) => res,
+            Err(_) => {
+                return Err(());
+            }
+        };
+    
+        match deck_num {
+            4=>{
+                if play_field.numb_4deck<MAX_4DECK{
+                   set_ship_helper(selection,&mut play_field.field)?; 
+                   play_field.numb_4deck+=1;
+                }
+            },
+            3=>{
+                if play_field.numb_3deck<MAX_3DECK{
+                   set_ship_helper(selection,&mut play_field.field)?; 
+    
+                   play_field.numb_3deck+=1;
+                }
+            },
+            2=>{
+                if play_field.numb_2deck<MAX_2DECK{
+                   set_ship_helper(selection,&mut play_field.field)?; 
+    
+                   play_field.numb_2deck+=1;
+                }
+            },
+            1=>{
+                if play_field.numb_1deck<MAX_1DECK{
+                   set_ship_helper(selection,&mut play_field.field)?; 
+    
+                   play_field.numb_1deck+=1; 
+                }
+            },
+            _=>(),
+        };
+    
+        Ok(())
+    }
+
+
 }
 
 
@@ -324,9 +339,9 @@ impl PrepWindow{
     pub fn place_ship(&mut self){
         let (rt, cl, rb, cr) = self.table.get_selection();
 
-        let field = &mut PLAYER_FIELD.lock().unwrap();
-
-        let _ = field.place_ship((rt, cl, rb, cr));
+        let mut field = &mut PLAYER_FIELD.lock().unwrap();
+       
+        let _ = PlayField::place_ship(&mut field,(rt, cl, rb, cr));
         
         let (n_1decks ,n_2decks,n_3decks,n_4decks)=field.get_ship_numb();
 
